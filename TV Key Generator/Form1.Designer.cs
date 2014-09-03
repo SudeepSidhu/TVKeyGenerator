e6a673e19c2b7b86bfaf482f5318ed673815d606
Sudeep Sidhu <sidhu.sudeep@gmail.com>
2014-09-03 00:32:27 -0500

    Everything \seems\ to work

---
+++ TV Key Generator/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TV_Key_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Location = new System.Drawing.Point(109, 41);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(139, 27);
+            this.btnPatch.TabIndex = 0;
+            this.btnPatch.Text = "Find + Install a key";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtOut.Location = new System.Drawing.Point(12, 16);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.Size = new System.Drawing.Size(332, 13);
+            this.txtOut.TabIndex = 1;
+            this.txtOut.TabStop = false;
+            this.txtOut.Text = "Ready to go!";
+            this.txtOut.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(356, 77);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.btnPatch);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "T:V Key Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPatch;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ TV Key Generator/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Threading;
+
+namespace TV_Key_Generator
+{
+    public partial class Form1 : Form
+    {
+        const string dllPath = "GameAISupport.dll";
+
+        [DllImport(dllPath)]
+
+        public static extern int GameAISupportQueryItem(String key);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+
+            if (File.Exists(dllPath))
+            {
+                txtOut.Text = "Finding a key...";
+
+                //because screw writing a background worker just so I can do a label update
+                ThreadPool.QueueUserWorkItem(new WaitCallback(delegate
+                {
+                    string key = KeyGenerator.generateKey();
+
+                    int retVal = GameAISupportQueryItem(key);
+
+                    if (retVal == 1)
+                    {
+                        txtOut.Invoke((MethodInvoker)(() => txtOut.Text = "Successfully installed \"" + key + "\""));
+                    }
+                    else
+                    {
+                        txtOut.Invoke((MethodInvoker)(() => txtOut.Text = "Problem installing \"" + key + "\" try again."));
+                    }
+                }));
+            }
+            else
+            {
+                MessageBoxEx.Show("GameAISupport.dll not found, please move this program to the Tribes Vengeance Bin folder, typically in " + ProgramFilesx86() + "\\VUGames\\Tribes Vengeance\\Program\\Bin", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        static string ProgramFilesx86()
+        {
+            if (8 == IntPtr.Size || (!String.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))))
+            {
+                return Environment.GetEnvironmentVariable("ProgramFiles(x86)");
+            }
+
+            return Environment.GetEnvironmentVariable("ProgramFiles");
+        }
+    }
+}
